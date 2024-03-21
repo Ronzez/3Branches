63c139913f7f81ee0cd5eeb175c293fd26f76f21
Ronzez <ronzez54@gmail.com>
2024-03-21 23:31:36 +0300

    Added factorial logic

--- WpfApp1/MathLogics/Factorial.cs
+++ WpfApp1/MathLogics/Factorial.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace WpfApp1.MathLogics
 {
-    internal class Factorial
+    public class Factorial
     {
+        public static int Calculate(int n)
+        {
+            int res = 1;
+            for (int i = 0; i < n; i++)
+            {
+                res *= i;
+            }
+            return res;
+        }
     }
 }
